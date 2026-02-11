25d69f8ee7c806f67816709d84447de6a56d26b0
carloshob <carloshenrique.ob@gmail.com>
2026-02-11 12:15:54 -0300

    feat(postgres): adicionar postgres ao projeto See: https://github.com/splor-mg/classificador-receita/issues/52 See: https://github.com/splor-mg/classificador-receita/issues/53 See: https://github.com/splor-mg/classificador-receita/tree/banco

--- classificador/settings.py
+++ classificador/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "classificador.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "classificador_receita",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
