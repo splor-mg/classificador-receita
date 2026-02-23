bc0bf042219ad594ab36d7623748d7cdc62069b2
carloshob <carloshenrique.ob@gmail.com>
2026-02-23 11:41:03 -0300

    fix(app): ajustar AppConfig.name para apps.core e settings.INSTALLED_APPS para apps.core.apps.CoreConfig See https://github.com/splor-mg/classificador-receita/issues/52 See https://github.com/splor-mg/classificador-receita/issues/53 See https://github.com/splor-mg/classificador-receita/tree/banco

--- classificador/settings.py
+++ classificador/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "core",
+    "apps.core.apps.CoreConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
