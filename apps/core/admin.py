e643421d86d7b2efd5d85a4ed63a25c34f83c057
carloshob <carloshenrique.ob@gmail.com>
2026-03-16 11:42:04 -0300

    feat(classificacao): implementação inicial do padrao crud ao model classificacao See https://github.com/splor-mg/classificador-receita/issues/52 See https://github.com/splor-mg/classificador-receita/issues/53 See https://github.com/splor-mg/classificador-receita/tree/banco

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -132,13 +132,93 @@ class SerieClassificacaoAdmin(
 
 
 @admin.register(Classificacao)
-class ClassificacaoAdmin(CoreChangeSaveFormSubmitMixin, BitemporalInactiveReadOnlyMixin, AutoExportAdminMixin, admin.ModelAdmin):
-    list_display = ['classificacao_id', 'classificacao_nome', 'serie_id', 'tipo_classificacao', 'numero_niveis', 'data_vigencia_inicio']
-    list_filter = [RegistroAtivoFilter, ClassificacaoIdFilter, 'tipo_classificacao', 'numero_niveis', 'serie_id', 'data_vigencia_inicio']
-    search_fields = ['classificacao_id', 'classificacao_nome', 'classificacao_descricao']
-    readonly_fields = ['data_registro_inicio', 'data_registro_fim']
-    date_hierarchy = 'data_vigencia_inicio'
-    raw_id_fields = ['serie_id', 'base_legal_tecnica_id']
+class ClassificacaoAdmin(
+    BitemporalAdminMixin,
+    BitemporalInactiveReadOnlyMixin,
+    BitemporalDateFormatMixin,
+    CoreChangeSaveFormSubmitMixin,
+    AutoExportAdminMixin,
+    admin.ModelAdmin,
+):
+    list_display = [
+        "classificacao_id",
+        "classificacao_nome",
+        "serie_id_raw",
+        "tipo_classificacao",
+        "numero_niveis",
+        "data_vigencia_inicio_fmt",
+        "data_vigencia_fim_fmt",
+        "data_registro_inicio_fmt",
+        "data_registro_fim_fmt",
+    ]
+    ordering = [
+        'classificacao_ref',
+        'data_vigencia_inicio',
+        'data_registro_inicio',
+    ]
+    list_filter = [
+        RegistroAtivoFilter,
+        ClassificacaoIdFilter,
+        "tipo_classificacao",
+        "numero_niveis",
+        "serie_id",
+        "data_vigencia_inicio",
+        "data_registro_inicio",
+    ]
+    search_fields = ["classificacao_id", "classificacao_nome", "classificacao_descricao"]
+    readonly_fields = ["data_registro_inicio_fmt", "data_registro_fim_fmt"]
+    date_hierarchy = "data_vigencia_inicio"
+    raw_id_fields = ["serie_id", "base_legal_tecnica_id"]
+
+    class Media:
+        js = ("core/admin_bitemporal_date_shortcuts.js",)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom = [
+            path(
+                "<path:object_id>/block/",
+                self.admin_site.admin_view(self.block_view),
+                name="core_classificacao_block",
+            ),
+            path(
+                "<path:object_id>/delete/",
+                self.admin_site.admin_view(self.delete_view),
+                name="core_classificacao_delete",
+            ),
+        ]
+        return custom + urls
+
+    def block_view(self, request, object_id):
+        handler = BlockHandler(self)
+        return handler.handle(request, object_id)
+
+    def delete_view(self, request, object_id):
+        handler = DeleteHandler(self)
+        return handler.handle(request, object_id)
+
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        if object_id:
+            extra_context["block_url"] = reverse(
+                "admin:core_classificacao_block",
+                args=[object_id],
+            )
+            extra_context["delete_url"] = reverse(
+                "admin:core_classificacao_delete",
+                args=[object_id],
+            )
+        return super().changeform_view(request, object_id, form_url, extra_context)
+
+    @admin.display(description="Série de Classificações")
+    def serie_id_raw(self, obj):
+        if obj.serie_id_id is None:
+            return ""
+        # Mostra apenas o identificador semântico da série, sem o nome.
+        try:
+            return obj.serie_id.serie_id
+        except Exception:
+            return obj.serie_id_id
 
 
 @admin.register(NivelHierarquico)
