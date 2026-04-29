ec67f7746fee7e1d6fcf4c047b40106a9351c7f9
carloshob <carloshenrique.ob@gmail.com>
2026-04-29 18:17:58 -0300

    feat(filtros): alterar nome do filtro por natureza de receita para melhorar UX See #18 See https://github.com/splor-mg/classificador-receita/tree/migracao

--- apps/core/admin_filters.py
+++ apps/core/admin_filters.py
@@ -17,7 +17,7 @@ from apps.core.models import BaseLegalTecnica, Classificacao, ItemClassificacao,
 SerieIdFilter = make_filter_local_id("serie_id", title="Identificador da Série")
 ClassificacaoIdFilter = make_filter_local_id("classificacao_id", title="Identificador da Classificação")
 NivelIdFilter = make_filter_local_id("nivel_id", title="Identificador do Nível")
-ItemIdFilter = make_filter_local_id("item_id", title="Identificador do Item")
+ItemIdFilter = make_filter_local_id("item_id", title="Código de Classificação")
 VersaoIdFilter = make_filter_local_id("versao_id", title="Identificador da Versão")
 VarianteIdFilter = make_filter_local_id("variante_id", title="Identificador da Variante")
 BaseLegalTecnicaIdFilter = make_filter_local_id("base_legal_tecnica_id", title="Identificador da Base Legal/Técnica")
